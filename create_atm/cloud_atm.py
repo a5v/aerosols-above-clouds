dc383e9dd0326786ab2ea54454b43342fa48b863
a5v <said2503@gmail.com>
2021-02-24 16:15:04 +0000

    improving cloud atmosphere

--- create_atm/cloud_atm.py
+++ create_atm/cloud_atm.py
@@ -21,16 +21,19 @@ if __name__ == '__main__':
 
     ##DISORT CONFIGURATION
 
-    umu0 = 0.698  #cosine of solar zenith angle (Default: 1.)
+    # umu0 = 0.698  #cosine of solar zenith angle (Default: 1.)
+    umu0 = 1.
     fbeam  = 1./umu0  # Ensures fluxes to be normalized to one
     phi0   = 0.0 #solar azimuth angle (Default: 0.)
-    albedo = 0 #surface albedo (Default: 0.1)
+    # albedo = 0 #surface albedo (Default: 0.1)
+    albedo = 0.1
 
-    phi = np.array([63.9]) #viewing azimuth angle where to output the RT fields (Default: 0.)
-    umu = np.array([0.883])  #cosine of viewing zenith angle where to output the RT fields (Default: 1.) 
-
+    # phi = np.array([63.9]) #viewing azimuth angle where to output the RT fields (Default: 0.)
+    phi = np.array([0.])
+    # umu = np.array([0.883])  #cosine of viewing zenith angle where to output the RT fields (Default: 1.) 
+    umu = np.array([1.])
     
-    #prnt   = np.array([True, True, True, False, True])
+    # prnt   = np.array([True, True, True, False, True])
     prnt   = np.array([True, False, False, False, False])
     maxmom = 1000
 
@@ -39,9 +42,9 @@ if __name__ == '__main__':
 
     #CREATE LAYERS
     ###three layer atmosphere
-    ##layer 0: 4-100km Rayleigh scattering
-    ##layer 1: 3-4km cloud and Rayleigh scattering layer
-    ##layer 2: 0-3km Rayleigh scattering
+    ##layer 0: 2-100km Rayleigh scattering
+    ##layer 1: 1-2km cloud and Rayleigh scattering layer
+    ##layer 2: 0-1km Rayleigh scattering
     
     N_layer  = 3 ## need to change this!!!!!!!!!!!!!!!!!!!!
     iphas  = np.ones(N_layer,dtype='int')*6
@@ -57,19 +60,19 @@ if __name__ == '__main__':
 
     ##CREATE RAYLEIGH LAYER (0)
     
-    dTau_R0_v = atmfunc.RdTau(wvl_v, atmfunc.h2p(4), atmfunc.h2p(100))
-    dTau_R0_ir = atmfunc.RdTau(wvl_ir, atmfunc.h2p(4), atmfunc.h2p(100))
+    dTau_R0_v = atmfunc.RdTau(wvl_v, atmfunc.h2p(2), atmfunc.h2p(100))
+    dTau_R0_ir = atmfunc.RdTau(wvl_ir, atmfunc.h2p(2), atmfunc.h2p(100))
 
     ##CREATE CLOUD & RAYLEIGH LAYER (1)
     
-    dTau_R1_v = atmfunc.RdTau(wvl_v, atmfunc.h2p(3), atmfunc.h2p(4))
-    dTau_R1_ir = atmfunc.RdTau(wvl_ir, atmfunc.h2p(3), atmfunc.h2p(4))
+    dTau_R1_v = atmfunc.RdTau(wvl_v, atmfunc.h2p(1), atmfunc.h2p(2))
+    dTau_R1_ir = atmfunc.RdTau(wvl_ir, atmfunc.h2p(1), atmfunc.h2p(2))
 
 
     dTau_range_cloud = [] # set optical thickness of cloud layer
 
-    for i in range(8):
-        a = 2**(i-2)
+    for i in range(30):
+        a = 2**(float(i-10)/2)
         x = np.array([a])
         dTau_range_cloud.append(x)
 
@@ -78,7 +81,7 @@ if __name__ == '__main__':
     ###READ IN WATER.CSV DATA FILE
     ###REFORMAT DATA TO BE USED BY DISORT AND SAVED
 
-    df_cloud = pd.read_csv('./aerosols/water.csv', header = None)
+    df_cloud = pd.read_csv('./aerosols/water2.csv', header = None)
     
     N_cloud = df_cloud.shape[1]/2
 
@@ -97,8 +100,8 @@ if __name__ == '__main__':
 
     ##CREATE RAYLEIGH LAYER (2)
     
-    dTau_R2_v = atmfunc.RdTau(wvl_v, atmfunc.h2p(0), atmfunc.h2p(3))
-    dTau_R2_ir = atmfunc.RdTau(wvl_ir, atmfunc.h2p(0), atmfunc.h2p(3))
+    dTau_R2_v = atmfunc.RdTau(wvl_v, atmfunc.h2p(0), atmfunc.h2p(1))
+    dTau_R2_ir = atmfunc.RdTau(wvl_ir, atmfunc.h2p(0), atmfunc.h2p(1))
 
 
     ###COMBINE TO CREATE ATMOSPHERE & CALL DISORT
@@ -106,7 +109,7 @@ if __name__ == '__main__':
     
     uu_save = []
     
-    for i in range(N_dTau_cloud): # iterate over range of cloud optical depth # N_dTau_aerosol
+    for i in range(N_dTau_cloud): # iterate over range of cloud optical depth # N_dTau_cloud
         dTau_atm_v = np.zeros((N_layer, 1))
         dTau_atm_v[0] = dTau_R0_v # insert optical depths in backwards
         dTau_atm_v[1] = atmfunc.comb_dTau(dTau_R1_v, dTau_range_cloud[i])
@@ -122,7 +125,7 @@ if __name__ == '__main__':
 
         uu_single_dTau.append(dTau_range_cloud[i][0]) # save optical thickness of the cloud
 
-        for j in range(N_cloud): # iterate over cloud data used # wvl_v # note: wavelength of light is important
+        for j in range(N_cloud): # iterate over cloud data used # N_cloud # note: wavelength of light is important
 
             w0_atm = np.zeros((N_layer, 1))
             w0_atm[0] = w0_R
@@ -144,17 +147,17 @@ if __name__ == '__main__':
             uu_single_dTau.append(uu[0][0][0])
 
 
-        for j in range(N_cloud): # iterate over cloud data used # wvl_ir # note: j+6 used!! 
+        for j in range(N_cloud): # iterate over cloud data used # wvl_ir # note: j+N_cloud used!! # N_cloud
 
             w0_atm = np.zeros((N_layer, 1))
             w0_atm[0] = w0_R
-            w0_atm[1] = atmfunc.comb_w0(dTau_R1_ir, dTau_range_cloud[i], w0_data_cloud[j+6]) # cloud data used
+            w0_atm[1] = atmfunc.comb_w0(dTau_R1_ir, dTau_range_cloud[i], w0_data_cloud[j+N_cloud]) # cloud data used
             w0_atm[2] = w0_R
     
             uphas_atm = np.zeros((N_layer, maxmom))
             uphas_atm[0] = uphas_R
             for k in range(maxmom):
-                uphas_atm[1][k] = atmfunc.comb_uphas(dTau_R1_ir, dTau_range_cloud[i][0], w0_data_cloud[j+6], uphas_R[0][k], uphas_data_cloud[j+6][k]) # cloud data used
+                uphas_atm[1][k] = atmfunc.comb_uphas(dTau_R1_ir, dTau_range_cloud[i][0], w0_data_cloud[j+N_cloud], uphas_R[0][k], uphas_data_cloud[j+N_cloud][k]) # cloud data used
             uphas_atm[2] = uphas_R
             
             [rfldir, rfldn, flup, dfdt, uavg, uu, albmed, trnmed] =\
@@ -170,7 +173,7 @@ if __name__ == '__main__':
 
 
 
-    name = './atmospheres/cloud_atm.csv'
+    name = './atmospheres/cloud_atm_1-2_long.csv'
         
     with open(name, 'w') as file:
         writer = csv.writer(file)
@@ -178,65 +181,3 @@ if __name__ == '__main__':
             writer.writerow(uu_save[i])
     
 
-    
-    
-
-    # ##CALLING DISORT
-    # rfldir_array = []
-    # rfldn_array = []
-    # flup_array = []
-    # dfdt_array = []
-    # uavg_array = []
-    # uu_array = []
-    # albmed_array = []
-    # trnmed_array = []
-
-
-    # for j in range(n_data):    
-
-    #     list = []
-        
-    #     for i in range (len(dTau_range)):
-    #         [rfldir, rfldn, flup, dfdt, uavg, uu, albmed, trnmed] =\
-    #             disort.run(dTau=dTau_range[i], iphas=iphas,  uphas=[p_array[j]], w0=[w0_array[j]], gg=gg,
-    #                        umu0=umu0, phi0=phi0, albedo=albedo, fbeam=fbeam,
-    #                        umu=umu, phi=phi, maxmom=maxmom, prnt=prnt)
-
-    #         # rfldir_array.append(rfldir)
-    #         # rfldn_array.append(rfldn)
-    #         # flup_array.append(flup)
-    #         # dfdt_array.append(dfdt)
-    #         # uavg_array.append(uavg)
-    #         list.append(uu)
-    #         # albmed_array.append(albmed)
-    #         # trnmed_array.append(trnmed)
-    #     #print(list)
-    #     uu_array.append(list)
-
-
-    # ##SAVE DATA
-    # uu_save =[]
-
-    # for j in range(len(uu_array)):
-    #     x = []
-
-    #     y = []
-    #     y.append(wvl_data[j])
-    #     y.append(effr_data[j])
-    #     x.append(y)
-        
-    #     for i in range(len(uu_array[j])):
-    #         x.append(uu_array[j][i][0][0][0])
-    #     uu_save.append(x)
-
-    # dTau_save = []
-
-    # for i in range(len(dTau_range)):
-    #     dTau_save.append(dTau_range[i][0])
-
-
-    # with open('./atmospheres/cloud_atm.csv', 'w') as file:
-    #     writer = csv.writer(file)
-    #     writer.writerow(dTau_save)
-    #     for i in range(len(uu_save)):
-    #         writer.writerow(uu_save[i])
